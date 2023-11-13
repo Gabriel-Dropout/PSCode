2210f4bddba32d9618f2babc46cd93ecc4b3fdc6
Powering111 <50047351+Powering111@users.noreply.github.com>
2023-11-13 22:41:07 +0900

    Create several commands

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,41 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('Extension PSCode activated.');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pscode" is now active!');
+	context.subscriptions.push(
+		vscode.commands.registerCommand('pscode.activate', () => {
+			vscode.window.showInformationMessage('Activate PSCode');
+	}));
+	context.subscriptions.push(
+		vscode.commands.registerCommand('pscode.deactivate', () => {
+			vscode.window.showInformationMessage('Dectivate PSCode');
+	}));
+	context.subscriptions.push(
+		vscode.commands.registerCommand('pscode.compile', () => {
+			vscode.window.showInformationMessage('Compile');
+	}));
+	context.subscriptions.push(
+		vscode.commands.registerCommand('pscode.test', () => {
+			vscode.window.showInformationMessage('Test');
+	}));
+	context.subscriptions.push(
+		vscode.commands.registerCommand('pscode.new', () => {
+			vscode.window.showInformationMessage('new problem');
+	}));
+	context.subscriptions.push(
+		vscode.commands.registerCommand('pscode.panic', () => {
+			vscode.window.showInformationMessage('give up problem');
+	}));
+	context.subscriptions.push(
+		vscode.commands.registerCommand('pscode.focus', () => {
+			vscode.window.showInformationMessage('focus mode on');
+	}));
+	context.subscriptions.push(
+		vscode.commands.registerCommand('pscode.distract', () => {
+			vscode.window.showInformationMessage('focus mode off');
+	}));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('pscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from PSCode!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
